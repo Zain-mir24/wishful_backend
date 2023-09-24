2506372a5a76cf4dbda77d03e268284edaff8110
noobcoder24 <zainnaeemk10@gmail.com>
2023-09-24 21:33:31 +0500

    new apis

--- src/Products/products.controllers.ts
+++ src/Products/products.controllers.ts
@@ -1,4 +1,12 @@
-import { Controller, Post, Body,Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 @Controller('Products')
 export class ProductController {
@@ -9,13 +17,36 @@ export class ProductController {
     @Body('description') descriptionTitle: string,
     @Body('Price') priceTitle: number,
   ): any {
-    const generatedId= this.productService.insertProduct(productTitle,descriptionTitle,priceTitle);
-    return {id:generatedId}
+    try {
+      const generatedId = this.productService.insertProduct(
+        productTitle,
+        descriptionTitle,
+        priceTitle,
+      );
+      return { id: generatedId };
+    } catch (e) {
+      throw new HttpException(
+        {
+          status: HttpStatus.FORBIDDEN,
+          error: 'This is a custom message',
+        },
+        HttpStatus.FORBIDDEN,
+        {
+          cause: e,
+        },
+      );
+    }
   }
 
   @Get()
-  getProduct(){
-    const data= this.productService.getProducts()
-    return data
+  getProduct() {
+    const data = this.productService.getProducts();
+    return data;
+  }
+  
+  @Get(":id")
+  getProductById(@Param('id') prodId:string) {
+    const data = this.productService.getProductById(prodId);
+    return data;
   }
 }
--- src/Products/products.service.ts
+++ src/Products/products.service.ts
@@ -1,17 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './product.model';
 
 @Injectable()
 export class ProductsService {
   private products: Product[] = [];
   insertProduct(title: string, description: string, price: number) {
-    let prodId = new Date().toString();
+    let prodId = Math.random().toString();
     const newProduct = new Product(prodId, title, description, price);
     this.products.push(newProduct);
 
     return this.products;
   }
-  getProducts(){
-    return [...this.products]
+  getProducts() {
+    return [...this.products];
+  }
+  getProductById(ProductId: string) {
+    const product = this.products.find((prod) => prod.id === ProductId);
+    if (!product) {
+      throw new NotFoundException();
+    }
+    return { ...product };
   }
 }
