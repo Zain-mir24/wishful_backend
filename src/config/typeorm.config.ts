d2a88a55f194d2f2f385757fd94e6e3746da16bb
noobcoder24 <zainnaeemk10@gmail.com>
2024-03-15 23:44:25 +0500

    changes in the vercel json

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,26 +1,27 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-
+var parse = require('pg-connection-string').parse;
 var dotenv = require('dotenv');
 var dotenvExpand = require('dotenv-expand');
 
 var myEnv = dotenv.config();
 const myvalue = dotenvExpand.expand(myEnv).parsed;
+const connectionOptions = parse(myvalue.POSTGRES_URL);
 
-const databaseUsername =myvalue?.DB_USER_NAME || "";
-const databasePassword =myvalue?.DB_PASSWORD || "";
-const databaseName =myvalue?.DB_NAME || "";
-const databaseHost =myvalue?.DB_HOST || "";
-const databasePort = myvalue?.DB_PORT || "";
-const databaseType=myvalue?.DB_TYPE || "";
-
+// const databaseUsername =myvalue.DB_USER_NAME;
+// const databasePassword =myvalue.DB_PASSWORD;
+// const databaseName =myvalue.DB_NAME;
+// const databaseHost =myvalue.DB_HOST;
+// const databasePort = myvalue.DB_PORT;
+const databaseType=myvalue.DB_TYPE;
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: databaseType,
-  host: databaseHost,
-  port: databasePort,
-  username: databaseUsername,
-  password: databasePassword,
-  database: databaseName,
+  host: connectionOptions.host,
+  port: connectionOptions.port,
+  username: connectionOptions.user,
+  password: connectionOptions.password,
+  database: connectionOptions.database,
+  ssl: require,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
 
